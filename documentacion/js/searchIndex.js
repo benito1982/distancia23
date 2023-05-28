d9e41d86202f90497ac791ed35e98cbb4d99427f
alberto <106930595+benito1982@users.noreply.github.com>
2023-05-28 09:19:45 +0200

    Add files via upload

---
+++ documentacion/js/searchIndex.js
@@ -0,0 +1,19 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\mostrarTexto\u0028\u0029",
+            "name": "mostrarTexto",
+            "summary": "Funci\u00F3n\u0020mostrarTexto\nEsta\u0020funci\u00F3n\u0020imprime\u0020en\u0020pantalla\u0020el\u0020texto\u0020que\u0020se\u0020pasa\u0020como\u0020par\u00E1metro",
+            "url": "namespaces/default.html#function_mostrarTexto"
+        },                {
+            "fqsen": "\\suma\u0028\u0029",
+            "name": "suma",
+            "summary": "Funci\u00F3n\u0020suma\nEsta\u0020funci\u00F3n\u0020suma\u0020dos\u0020n\u00FAmeros\u0020pasados\u0020como\u0020par\u00E1metro",
+            "url": "namespaces/default.html#function_suma"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
